81d7b08ea467c7cbde52583cd2a67ec41f534084
KaiZhou <zhoukaisspu@163.com>
2018-09-23 15:58:58 +0800

    Add test code for "insert_sort"

--- chapter_2/insert_sort.hpp
+++ chapter_2/insert_sort.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstddef>
 
-void insert_sort(int* a, std::size_t length)
+void insert_sort(int* a, int length)
 {
 	if (length <= 1)
 		return;
@@ -14,4 +14,38 @@ void insert_sort(int* a, std::size_t length)
 		}
 		a[index + 1] = key;
 	}
+}
+
+void insert_sort_test()
+{
+	std::cout << "<<<<<<<<<<<<start insert sort test>>>>>>>>>>>>>>"
+		<< std::endl;
+	int a[10] = { 2,1,3,5,6,3,8,1,23,1 };
+	int sort_a[10] = { 1,1,1,2,3,3,5,6,8,23 };
+	std::cout << "insert sort:       ";
+	for (auto& j : a) {
+		std::cout << j << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "correctness result:";
+	for (auto& j : sort_a) {
+		std::cout << j << " ";
+	}
+	std::cout << std::endl;
+	insert_sort(a, 10);
+	std::cout << "actual result:     ";
+	for (auto& j : a) {
+		std::cout << j << " ";
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < 10; ++i) {
+		if (a[i] != sort_a[i]) {
+			std::cout << "[ERROR]: insert sort error!"
+				<< std::endl;
+			return;
+		}
+	}
+	std::cout << "[INFO]: insert sort correct" << std::endl;
+	std::cout << "<<<<<<<<<<<<<<<<end insert sort test>>>>>>>>>>>>>>>>"
+		<< std::endl;
 }
